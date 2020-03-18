9cff1bb3f8ce6b7910e57a2f04dad7d7d7831f26
Mariya Sergeeva <46738637+islamariya@users.noreply.github.com>
2020-03-18 16:32:30 +0500

    Add files via upload

--- ss_server_handler.py
+++ ss_server_handler.py
@@ -4,30 +4,47 @@ WSGI сервер (? или простой HTTP)
 - get: время освобождения манипулятора (response - str в секундах длительность занятости киоска
 - put: новый заказ ref_id
 """
-import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
+
+
+HOST_NAME = "localhost"
+PORT_NUMBER = 8080
+
+class SS_server_handler(BaseHTTPRequestHandler):
+    """Класс обертка для обработки запросов, логика обработки описывается в дочернх классах"""
+    def do_head(self):
+        """Описывает формирование заголовка ответа для* запроса"""
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_post(self):
+        pass
 
 
 def start_ss_server():
     """Описать сервер"""
-    serser_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serser_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serser_socket.bind(("localhost", 5001))
-    serser_socket.listen()
 
-    while True:
-        client_socket, adress = serser_socket.accept()
-        print("connection from", adress)
-
-        while True:
-            ss_request = client_socket.recv(4096)
-
-            if not ss_request:
-                break
-            else:
-                responce = "Preparation time is 673628 sec".encode()
-                client_socket.send(responce)
-
-        client_socket.close()
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # server_socket.bind(("localhost", 5001))
+    # server_socket.listen(1)
+    #
+    # while True:
+    #     client_socket, adress = server_socket.accept()
+    #     print("connection from", adress)
+    #
+    #     while True:
+    #         ss_request = client_socket.recv(4096)
+    #
+    #         if not ss_request:
+    #             break
+    #         else:
+    #             responce = "Preparation time is 673628 sec".encode()
+    #             client_socket.send(responce)
+    #
+    #     client_socket.close()
 
 
 def fetch_time_till_ready_to_cook():
--- system_start.py
+++ system_start.py
@@ -1,15 +1,57 @@
 """Этот модуль запускает работу. Работа должна быть асинхронной, модуль запускает и управляет потоками внутри(?)"""
+import asyncio
+import time
+import random
 
-from ss_server_handler import start_ss_server
-
+# from ss_server_handler import start_ss_server
 from main_order_handler import TodaysOrders
+from settings import QT_DISH_PER_ORDER
 
 
-def start_working():
-    """В этой функции должны быть все основные действия при запуске и работе. Это отдельные потоки"""
-    todays_orders = TodaysOrders()
-    start_ss_server()
+async def ss_server():
+    """Это курутина запускает сервер, мониторит и обрабатывает сообщения от SS
+    ВОПРОС: запуск сервера должен быть отдельно или тут?
+     - уведомление о новом заказе
+     - запрос оставшегося времени работы
+     """
+    while True:
+        print("Работает ss_server", time.time())
+        n = random.randint(1, 50)
+        print("SS Ждет", n)
+        await asyncio.sleep(n)
+        new_order = {"refid": (23+n), "dishes": [(2, 4, 6, 7), (1, 2, 4, 5)]}
+        today_orders.create_new_order(new_order, QT_DISH_PER_ORDER)
+        print("1 sec ss", time.time())
+
+
+async def controllers_alert_handler():
+    """Эта курутина обрабатывает уведомления от контроллеров: отказ оборудования и qr код """
+    while True:
+        print("Работает controllers_alert_handler", time.time())
+        await asyncio.sleep(2)
+        print("2 sec controllers_alert_handler", time.time())
+
+
+
+async def cooking():
+    """Эта курутина обеспеивает вызов методов по приготовлению блюд и другой важной работе"""
+    while True:
+        print("Работает cooking", time.time())
+        await asyncio.sleep(3)
+        print("Остались печи", today_orders.oven_avalable)
+        print("Это заказы в current_orders_proceed", today_orders.current_orders_proceed)
+        print("3 sec cooking", time.time())
+
+
+async def start_working():
+    """В этой функции курутины формируются в таски"""
+    ss_task = asyncio.create_task(ss_server())
+    controllers_task = asyncio.create_task(controllers_alert_handler())
+    cooking_task = asyncio.create_task(cooking())
+
+    await asyncio.gather(ss_task, controllers_task, cooking_task)
 
 
 if __name__ == "__main__":
-    start_working()
+    today_orders = TodaysOrders()
+    asyncio.run(start_working())
