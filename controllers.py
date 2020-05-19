f245c6000ca8d001fde2f483cefd2ce36c6e3e01
Mariya Sergeeva <46738637+islamariya@users.noreply.github.com>
2020-05-19 14:15:25 +0500

    update

--- controllers.py
+++ controllers.py
@@ -1,5 +1,48 @@
 import asyncio
 import random
+import time
+
+from pydispatch import Dispatcher
+
+
+class ControllersEvents(Dispatcher):
+    """
+    Dispatcher for controller event handlers. DON'T CREATE instances of this class, use
+    cntrls_events.
+    """
+    _events_ = ['qr_scanned', 'hardware_status_changed']
+
+    def qr_scanned(self, _params):
+        self.emit('qr_scanned', params=_params)
+
+    def hardware_status_changed(self, _unit_name, _status):
+        self.emit('hardware_status_changed', unit_name=_unit_name, status=_status)
+
+
+cntrls_events = ControllersEvents()
+
+async def event_generator(cntrls_events):
+
+    async def qr_code_scanning_alarm(cntrls_events):
+        print("Сработало событие qr код", time.time())
+        params = {"ref_id": 65, "pickup":1}
+        cntrls_events.qr_scanned(params)
+
+    async def hardware_status_changed(cntrls_events):
+        print("Сработало событие поломка печи", time.time())
+        cntrls_events.hardware_status_changed('owen_cell_2', 'broken')
+
+    while True:
+        await asyncio.sleep(2)
+        print("Выбираем событие", time.time())
+        options = [qr_code_scanning_alarm, hardware_status_changed]
+        my_choice = random.randint(0, 1)
+        what_happened = options[my_choice]
+        await what_happened(cntrls_events)
+        n = random.randint(1, 10)
+        print("Trouble-maker засыпает на", n, "сек в ", time.time())
+        await asyncio.sleep(n)
+        print("Trouble-maker снова с нами", time.time())
 
 
 class Movement(object):
--- start_PBM.py
+++ start_PBM.py
@@ -3,9 +3,11 @@
 import asyncio
 import random
 import time
-from aiohttp import web
 from contextvars import ContextVar
 
+from aiohttp import web
+
+from controllers import cntrls_events, event_generator
 
 # from ss_server_handler import new_order_handler
 from ss_server_handler import create_server
@@ -14,13 +16,36 @@ from PBM_main import PizzaBotMain
 from settings import QT_DISH_PER_ORDER
 
 
-async def controllers_alert_handler():
-    """Эта курутина обрабатывает уведомления от контроллеров: отказ оборудования и qr код
-     Можно тут запустить методы мониторинга Арсения."""
-    while True:
-        print("Переключились в контролеры", time.time())
-        await asyncio.sleep(5)
-        print("Контролеры отработали")
+async def hello_from_qr_code():
+    print("QR код обработан", time.time())
+
+
+async def hello_from_broken_oven():
+    print("Изменение статуса оборудования обработано", time.time())
+
+
+async def controllers_alert_handler(cntrls_events):
+    """Эта курутина обрабатывает уведомления от контроллеров: отказ оборудования и qr код """
+
+    print("Переключились в контролеры", time.time())
+
+    async def wait_for_qr_code(cntrls_events):
+        event_name = "qr_scanned"
+        event = cntrls_events.get_dispatcher_event(event_name)
+        while True:
+            result = await event
+            await hello_from_qr_code()
+
+    async def wait_for_hardware_status_changed(cntrls_events):
+        event_name = "hardware_status_changed"
+        event = cntrls_events.get_dispatcher_event(event_name)
+        while True:
+            result = await event
+            await hello_from_broken_oven()
+
+    qr_event_waiter = asyncio.create_task(wait_for_qr_code(cntrls_events))
+    status_change_waiter = asyncio.create_task(wait_for_hardware_status_changed(cntrls_events))
+    await asyncio.gather(qr_event_waiter, status_change_waiter)
 
 
 async def cooking(today_orders):
@@ -57,7 +82,7 @@ async def cooking(today_orders):
             print()
 
 
-async def create_tasks(app, today_orders):
+async def create_tasks(app, today_orders, cntrls_events):
     # добавляем асинхонный запуск сервера
     runner = web.AppRunner(app)
     await runner.setup()
@@ -66,9 +91,11 @@ async def create_tasks(app, today_orders):
     print("Сервер запущен на 127.0.0.1:8080")
 
     # запускаем работу бесконечных тасков cooking и controllers
-    controllers_task = asyncio.create_task(controllers_alert_handler())
+    # events разовый такс для проверки работоспособности и имитации работы контроллеров
+    events = asyncio.create_task(event_generator(cntrls_events))
+    controllers_task = asyncio.create_task(controllers_alert_handler(cntrls_events))
     cooking_task = asyncio.create_task(cooking(today_orders))
-    await asyncio.gather(controllers_task, cooking_task)
+    await asyncio.gather(controllers_task, cooking_task, events)
 
 
 def start(equipment_data, recipes):
@@ -77,6 +104,18 @@ def start(equipment_data, recipes):
     today_orders = PizzaBotMain(equipment_data, recipes)
     print("Начинается код start cooking")
     app = create_server()
-    app["today_orders"]=today_orders
-    asyncio.run(create_tasks(app, today_orders))
+    app["today_orders"] = today_orders
+    asyncio.run(create_tasks(app, today_orders, cntrls_events))
 
+# # Event handlers
+# def qr_handler(guid):
+#     print('QR has been scanned. GUID:', guid)
+#
+#
+# def status_handler(unit_name, status):
+#     print(unit_name, 'status changed. Now:', status)
+#
+#
+# # bind handlers to a corresponding event
+# cntrls_events.bind(qr_scanned=qr_handler)
+# cntrls_events.bind(hardware_status_changed=status_handler)
